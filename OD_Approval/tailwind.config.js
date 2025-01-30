4edd42f19bde40c5b5aa7db16888485f4564daa7
kanigasree-s <kanigasree2005s@gmail.com>
2025-01-31 05:21:03 +0530

    Add files via upload

---
+++ OD_Approval/tailwind.config.js
@@ -0,0 +1,24 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+        fadeIn3s:'fadeIn 3s ease-in-out'
+      },
+    },
+  },
+  plugins: [],
+});
+
